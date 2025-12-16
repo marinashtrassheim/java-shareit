f77b49abd014aa4e327f14e6280abd1edcef70fc
Marina Shtrassheim <mf.shtrasgeym@mail.ru>
2025-12-16 21:28:24 +0700

    fix unused import2

--- src/main/java/ru/practicum/shareit/item/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/item/ItemRequestDto.java
@@ -1,18 +1,12 @@
 package ru.practicum.shareit.item;
 
-import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class ItemRequestDto {
-    @NotNull(message = "Название не может быть пустым")
     private String name;
-
-    @NotNull(message = "Описание не может быть пустым")
     private String description;
-
-    @NotNull(message = "Необходимо заполнить поле доступность")
     private Boolean available;
-
     private Integer requestId;
 }
