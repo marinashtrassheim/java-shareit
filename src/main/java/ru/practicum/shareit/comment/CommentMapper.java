cbd2629a7cffe2b225b0b4e6a7821d9e47ddb345
Marina Shtrassheim <mf.shtrasgeym@mail.ru>
2025-12-19 21:52:54 +0700

    update by comments2

--- src/main/java/ru/practicum/shareit/comment/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/comment/CommentMapper.java
@@ -7,10 +7,8 @@ import ru.practicum.shareit.item.ItemMapper;
 import ru.practicum.shareit.user.UserEntity;
 import ru.practicum.shareit.user.UserMapper;
 
-@Mapper(componentModel = "spring",
-        uses = {ItemMapper.class, UserMapper.class})
+@Mapper(componentModel = "spring", uses = {ItemMapper.class, UserMapper.class})
 public interface CommentMapper {
-
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "item", source = "item")
     @Mapping(target = "author", source = "author")
@@ -18,6 +16,7 @@ public interface CommentMapper {
     CommentEntity toEntity(CommentRequestDto requestDto,
                            ItemEntity item,
                            UserEntity author);
+
     @Mapping(target = "itemId", source = "item.id")
     @Mapping(target = "authorId", source = "author.id")
     @Mapping(target = "authorName", source = "author.name")
